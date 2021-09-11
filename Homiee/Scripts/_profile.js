5950c435f544260af3560330e92e747cad3d6939
sjalim <sjalim71@gmail.com>
2021-09-11 10:55:42 +0600

    templete

---
+++ Homiee/Scripts/_profile.js
@@ -0,0 +1,38 @@
+﻿
+    function EditButton() {
+    var firstNameFiled = document.getElementById("firstName");
+    var lastNameFiled = document.getElementById("lastName");
+    var emailFiled = document.getElementById("email");
+    var phoneFiled = document.getElementById("phone");
+    var countryFiled = document.getElementById("country");
+    var divisionFiled = document.getElementById("division");
+    var cityFiled = document.getElementById("city");
+    var area = document.getElementById("area");
+    var addressExtension = document.getElementById("addressExtension");
+    var cancelButton = document.getElementById("cancelButton");
+    var submitButton = document.getElementById("submitButton");
+
+    console.log("edit button");
+        
+
+        }
+
+    function SubmitButton() {
+
+    }
+
+    function CancelButton() {
+
+    }
+
+
+
+        //function runWhenReady() {
+        //    if (window.$)
+        //        EditButton();
+        //    else
+        //        setTimeout(runWhenReady, 100);
+        //}
+        //runWhenReady();
+
+
