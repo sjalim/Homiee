9e3e6ebc74c40fe82f840919ee39a3e06a3cb8df
MahdiHassanKhan <mahdihassankhan10@gmail.com>
2021-09-20 23:09:45 +0600

    commit from mahdi

---
+++ Homiee/Controllers/HostLandingController.cs
@@ -0,0 +1,49 @@
+﻿using Homiee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homiee.Controllers
+{
+    public class HostLandingController : Controller
+    {
+        // GET: HostLanding
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddPlace()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPlace(HostInfo hostinfo)
+        {
+            if(ModelState.IsValid)
+            {
+                db.HostInfos.Add(hostinfo);
+            }
+            return RedirectToAction("Index", "HostLanding");
+
+        }
+
+
+        public ActionResult Preview()
+        {
+            return View();
+        }
+
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Homiee/Models/HostInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homiee.Models
+{
+    public class HostInfo
+    {
+        [Key]
+        public int HostInfoID { get; set; }
+        public int Room { get; set; }
+        public int NumRooms { get; set; }
+        public int NumKitchens { get; set; }
+        public int NumWash { get; set; }
+        public int NumBalconys { get; set; }
+        public string AdditionalFeatures { get; set; }
+
+        public string CountryName { get; set; }
+        public string StreetName { get; set; }
+        public string CityName { get; set; }
+        public string StateName { get; set; }
+        public int PostCode { get; set; }
+
+        public string HostRules { get; set; }
+        public int MinStay { get; set; }
+        public int MaxStay { get; set; }
+        public double Price { get; set; }
+        public string Offer { get; set; }
+        public string RoomCaption { get; set; }
+       // public byte[] AddFile { get; set; }
+
+        public virtual User UserID { get; set; }
+
+    }
+}
