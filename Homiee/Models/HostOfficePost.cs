f097fd7640a097250c25c32a9b7e3e9f7378d64a
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2021-10-06 04:33:04 +0600

    conflict resolve 05

--- Homiee/Models/HostOfficePost.cs
+++ Homiee/Models/HostOfficePost.cs
@@ -7,24 +7,18 @@ namespace Homiee.Models
 {
     public class HostOfficePost
     {
-
+        [Key]
         public int HostOfficePostID { get; set; }
 
         public string Title { get; set; }
+
         public double SpaceSize { get; set; }
 
         public double Price { get; set; }
-
-
         public int PaymentType { get; set; }
-
         public string Offer { get; set; }
-
         public string AddFile { get; set; }
-
         public string RoomCaption { get; set; }
-       
-
         public virtual User User { get; set; }
     }
 }
